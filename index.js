b51978edf3f93269813722394cc98114366013ba
belleomoraes <iolliveira.moraes@gmail.com>
2022-09-07 21:25:42 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from "cors";
+import joi from "joi";
+import { MongoClient, ObjectId } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db;
+mongoClient.connect().then(() => {
+  db = mongoClient.db("mywallet");
+});
+
+app.listen(5000, () => console.log("Listening on port 5000"));
