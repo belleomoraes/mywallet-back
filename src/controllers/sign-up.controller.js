66fc01c9b36ad2802e28eb47ab91a0dd6a0c14d0
belleomoraes <iolliveira.moraes@gmail.com>
2022-09-11 20:46:19 -0300

    with validations schema on controller

--- src/controllers/sign-up.controller.js
+++ src/controllers/sign-up.controller.js
@@ -1,10 +1,22 @@
+import joi from "joi";
 import db from "../database/db.js";
 import bcrypt from "bcrypt";
 
+const signupSchema = joi.object({
+  name: joi.string().trim().required(),
+  email: joi.string().trim().email().required(),
+  password: joi.number().required(),
+  confirmPassword: joi.number().required(),
+});
+
 async function signUp(req, res) {
   const { name, email, password, confirmPassword } = req.body;
-
+  const validation = signupSchema.validate(req.body, { abortEarly: false });
   const isEmailExists = await db.collection("user").findOne({ email });
+
+  if (validation.error) {
+    return res.status(422).send({ message: validation.error.message });
+  }
 
   if (isEmailExists) {
     return res.status(409).send({ message: "Este e-mail já está cadastrado" });
